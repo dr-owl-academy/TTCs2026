6652d65e5d005b8cf85d5a7f27acae70216520ce
eason <easonchien.tw@gmail.com>
2026-07-24 15:50:03 -0700

    auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Eason_Auton_Starter.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Eason_Auton_Starter")
+public class Eason_Auton_Starter  extends OpMode {
+
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
+    ElapsedTime timer = new ElapsedTime();
+
+    double drivePower = 0.50;
+    double leftFrontPower;
+    double rightFrontPower;
+    double leftBackPower;
+    double rightBackPower;
+
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void start() {
+        // Start the timer when the driver presses Play.
+        timer.reset();
+    }
+
+    @Override
+    public void loop() {
+
+        double time = timer.seconds();
+
+        if (time < 1.5) {
+
+           mecanumDrive(drivePower,0,0);
+
+        } else if (time < 2.0) {
+
+            mecanumDrive(0,0,0);
+
+        } else if (time < 3.8) {
+
+            mecanumDrive(0,drivePower,0);
+
+        } else if (time < 4.3) {
+
+            mecanumDrive(0,0,0);
+
+        } else if (time < 5.8) {
+
+            mecanumDrive(-drivePower,0,0);
+
+        } else if (time < 6.3) {
+
+            mecanumDrive(0,0,0);
+
+        } else if (time < 8.1) {
+
+            mecanumDrive(0,-drivePower,0);
+
+        } else {
+
+            mecanumDrive(0,0,0);
+
+        }
+        telemetry.addData("SECONDS",time);
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+    void mecanumDrive(double forward, double strafe, double rotate){
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontPower = (forward + strafe + rotate) / denominator;
+        rightFrontPower = (forward - strafe - rotate) / denominator;
+        leftBackPower = (forward - strafe + rotate) / denominator;
+        rightBackPower = (forward + strafe - rotate) / denominator;
+
+        frontLeft.setPower(leftFrontPower);
+        frontRight.setPower(rightFrontPower);
+        backLeft.setPower(leftBackPower);
+        backRight.setPower(rightBackPower);
+
+    }
+}
