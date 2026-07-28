1e665925a07f5c16a055bed4ad52aa20e3361d85
danielthai <daniel.thai2011@gmail.com>
2026-07-28 15:05:32 -0700

    Working on new constant #2 (Removing old numbers and replacing them, also renaming original constants to old constants and constants2 to constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -8,18 +8,14 @@ import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-@Disabled
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(8.4)
-            .forwardZeroPowerAcceleration(-34.34415518547419)
-            .lateralZeroPowerAcceleration(-54.76122077)
-    .translationalPIDFCoefficients(new PIDFCoefficients(0.3,0,0.0001,0.01))
-    .headingPIDFCoefficients(new PIDFCoefficients(0.6,0,0.2,0.01));
+    public static FollowerConstants followerConstants = new FollowerConstants().mass(7.05);
+
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("frontRight")
@@ -29,13 +25,12 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(59.146876858288226)
-            .yVelocity(50.5002934814);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(1.25)
-            .strafePodX(-4.62)
+            .forwardPodY(1.5)
+            .strafePodX(-7.5)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
@@ -49,6 +44,7 @@ public class Constants {
             0.27,
             0.85);
 
+
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pinpointLocalizer(localizerConstants)
@@ -57,3 +53,4 @@ public class Constants {
                 .build();
     }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Oldconstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Oldconstants.java
@@ -13,13 +13,13 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-public class Constants2 {
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(7.05)
+@Disabled
+public class Oldconstants {
+    public static FollowerConstants followerConstants = new FollowerConstants().mass(8.4)
             .forwardZeroPowerAcceleration(-34.34415518547419)
             .lateralZeroPowerAcceleration(-54.76122077)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.3,0,0.0001,0.01))
-            .headingPIDFCoefficients(new PIDFCoefficients(0.6,0,0.2,0.01));
+    .translationalPIDFCoefficients(new PIDFCoefficients(0.3,0,0.0001,0.01))
+    .headingPIDFCoefficients(new PIDFCoefficients(0.6,0,0.2,0.01));
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("frontRight")
@@ -34,8 +34,8 @@ public class Constants2 {
             .yVelocity(50.5002934814);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(1.5)
-            .strafePodX(-7.5)
+            .forwardPodY(1.25)
+            .strafePodX(-4.62)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
@@ -57,4 +57,3 @@ public class Constants2 {
                 .build();
     }
 }
-
