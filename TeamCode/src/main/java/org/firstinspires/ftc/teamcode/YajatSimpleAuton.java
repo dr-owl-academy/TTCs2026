192bb7bdda8b950b55dc88be83b5a7b840c66ada
yajatpramod <yajatpramod1211@gmail.com>
2026-07-29 14:52:42 -0700

    fixing errors4 ALMOST DONE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/YajatSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/YajatSimpleAuton.java
@@ -56,6 +56,33 @@ public class YajatSimpleAuton extends OpMode {
 
     }
 
+    @Override
+    public void loop() {
+
+        // Pedro must update every loop.
+        follower.update();
+
+        // Update the autonomous FSM.
+        autonomousPathUpdate();
+
+        Pose currentPose = follower.getPose();
+
+        telemetry.addData("X", currentPose.getX() );
+
+        telemetry.addData("Y", currentPose.getY() );
+
+        telemetry.addData("Heading", Math.toDegrees(currentPose.getHeading()));
+
+        telemetry.addData("State", AutoState );
+
+        if (AutoState == AutoState.COMPLETE) {
+
+            telemetry.addLine("Autonomous complete" );
+        }
+
+        telemetry.update();
+    }
+
 
 
 
