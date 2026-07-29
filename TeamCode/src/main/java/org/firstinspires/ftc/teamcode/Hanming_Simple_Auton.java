c18ff5a549f4498c0fee25938f85bdd1918d0106
hwgalaxyguardians <wanghmnet@gmail.com>
2026-07-29 14:05:45 -0700

    simple auton start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hanming_Simple_Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hanming_Simple_Auton.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Hanming_Simple_Auton")
 public class Hanming_Simple_Auton {
+}
+
+    private enum Autostate {
+        Init_turn,
+        Wait_turn,
+        Init_drive,
+        Wait_drive,
+        Complete
 }
 
