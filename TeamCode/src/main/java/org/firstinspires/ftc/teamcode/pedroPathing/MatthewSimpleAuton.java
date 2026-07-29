2370dabbe7fc48ed85e6dc8cc7bbd26c2fa53fb0
IMMORTL225 <lilsci2013@gmail.com>
2026-07-29 14:27:14 -0700

    Matthews Code commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
@@ -4,6 +4,9 @@ public class MatthewSimpleAuton {
 
     private enum Autostate{
         START_TURN_TO_180,
+        WAIT_FOR_TURN_TO_180,
+        START_DRIVE_TO_TARGET,
+        WAIT_FOR_DRIVE_TO_TARGET
 
     }
 }
