b33cc5b04bff8703e46e9cb585712e82d3be6763
yajatpramod <yajatpramod1211@gmail.com>
2026-07-29 14:44:15 -0700

    fixing errors2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/YajatSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/YajatSimpleAuton.java
@@ -10,7 +10,7 @@ import com.pedropathing.paths.PathChain;
 @Autonomous(name = "Simple Auton Code Yajat")
 public class YajatSimpleAuton extends OpMode {
     //now we put the things in order ig
-    private enum AutoState{
+    private enum AutoState {
         START_TURN_TO_180,
 
         WAIT_FOR_TURN,
@@ -18,24 +18,31 @@ public class YajatSimpleAuton extends OpMode {
         WAIT_FOR_TARGET,
         COMPLETE
     }
-}
 
-private Follower follower;
 
-//use pathchaining
-private PathChain driveToTarget;
+    private Follower follower;
+
+    //use pathchaining
+    private PathChain driveToTarget;
 
 //did somthing i think. also lots of errors
 
-private YajatSimpleAuton.AutoState = YajatSimpleAuton.AutoState.START_TURN_TO_180;
+    private YajatSimpleAuton.AutoState = YajatSimpleAuton.AutoState.START_TURN_TO_180;
 
-private static final pose START_POSE = new Pose(72,72,Math.toRadians());
+    private static final Pose START_POSE = new Pose(72, 72, Math.toRadians());
 
-private static final pose DRIVE_START_POSE = new Pose(72,72,180);
+    private static final Pose DRIVE_START_POSE = new Pose(72, 72, 180);
 
-private static final pose TARGET_POSE = new Pose(24,72,180);
+    private static final Pose TARGET_POSE = new Pose(24, 72, 180);
 
 //i think i did something so the bot knows where to go
 
-@Override
-public void init() {}
+    @Override
+    public void init() {
+    }
+
+
+
+
+
+}
