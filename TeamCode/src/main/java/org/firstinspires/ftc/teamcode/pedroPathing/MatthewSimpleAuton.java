4584f3cc8c0bf25dfd7ab096b5af2b40f057359d
IMMORTL225 <lilsci2013@gmail.com>
2026-07-29 14:35:07 -0700

    Matthews Code commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 
 public class MatthewSimpleAuton {
 
