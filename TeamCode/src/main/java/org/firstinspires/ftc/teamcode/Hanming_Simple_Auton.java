6c1a4dccdf49a19a862da8342d0cdf8bbc794c0a
hwgalaxyguardians <wanghmnet@gmail.com>
2026-07-29 14:30:14 -0700

    simple auton start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hanming_Simple_Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hanming_Simple_Auton.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous(name = "Hanming_Simple_Auton")
 public class Hanming_Simple_Auton {
@@ -14,6 +21,14 @@ public class Hanming_Simple_Auton {
         Complete
 }
 
-private Bot follower;
-private path pathChain;
-private
+    private Follower follower;
+    private path pathChain;
+    private AutoState autoState = AutoState.Init_turn;
+    private static final Pose Start_Pose = new Pose(72,72,Math.toRadians(90) );
+    private static final Pose Drive_Start_Pose = new Pose(72,72,Math.toRadians(180));
+    private static final Pose Complete_Pose = new Pose(24,72, Math.toRadians(180));
+
+    @Override
+    public void init() {
+
+        follower =
