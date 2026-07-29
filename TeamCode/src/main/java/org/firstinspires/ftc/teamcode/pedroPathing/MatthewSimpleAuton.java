97681d635632232fb3376659e72d39af3f6edb4e
IMMORTL225 <lilsci2013@gmail.com>
2026-07-29 14:36:07 -0700

    Matthews Code commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
@@ -23,4 +23,10 @@ public class MatthewSimpleAuton {
     //use PathChain for each path segment
     private PathChain driveToTarget;
 
+    // Starting FSM state.
+    private AutoState autoState = AutoState.START_TURN_TO_180;
+
+    private static final Pose START_POSE = new Pose(72,72,Math.toRadians(90) );
+    private static final Pose DRIVE_START_POSE = new Pose(72,72,Math.toRadians(180));
+    private static final Pose TARGET_POSE = new Pose(24,72, Math.toRadians(180));
 }
