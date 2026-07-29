f213e21d131fc993daa0d52100625d857836968f
IMMORTL225 <lilsci2013@gmail.com>
2026-07-29 14:29:59 -0700

    Matthews Code commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MatthewSimpleAuton.java
@@ -6,7 +6,10 @@ public class MatthewSimpleAuton {
         START_TURN_TO_180,
         WAIT_FOR_TURN_TO_180,
         START_DRIVE_TO_TARGET,
-        WAIT_FOR_DRIVE_TO_TARGET
+        WAIT_FOR_DRIVE_TO_TARGET,
+        COMPLETE
 
     }
+
+
 }
