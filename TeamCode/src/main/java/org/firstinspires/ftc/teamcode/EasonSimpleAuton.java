76f4281dc2e96726027f00fffec575b2fdc715ee
eason <easonchien.tw@gmail.com>
2026-07-29 15:17:08 -0700

    pidf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasonSimpleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasonSimpleAuton.java
@@ -23,19 +23,26 @@ public class EasonSimpleAuton extends OpMode {
         START_TURN_1,
         WAIT_FOR_TURN_1,
         START_DRIVE_TO_TARGET_1,
-        WAIT_FOR_DRIVE_TO_TARGET,
+        WAIT_FOR_DRIVE_TO_TARGET_1,
+        START_TURN_2,
+        WAIT_FOR_TURN_2,
+        START_DRIVE_TO_TARGET_2,
+        WAIT_FOR_DRIVE_TO_TARGET_2,
         COMPLETE
     }
     //Naming Follower
     private Follower follower;
     //Naming PathChain
     private PathChain driveToTarget;
+    private PathChain driveToTarget_2;
     //Start State
     private AutoState autoState = AutoState.START_TURN_1;
     //Show Action
     private static final Pose START_POSE = new Pose(72,72,Math.toRadians(90));
-    private static final Pose DRIVE_START_POSE = new Pose(72,72,Math.toRadians(180));
-    private static final Pose TARGET_POSE = new Pose(24,72,Math.toRadians(180));
+    private static final Pose DRIVE_START_POSE_1 = new Pose(72,72,Math.toRadians(180));
+    private static final Pose TARGET_POSE_1 = new Pose(24,72,Math.toRadians(180));
+    private static final Pose DRIVE_START_POSE_2 = new Pose(24,72,Math.toRadians(-150));
+    private static final Pose TARGET_POSE_2 = new Pose(48,24,Math.toRadians(-150));
     private static final double INTAKE_VELOCITY = 2000;
     @Override
     public void init(){
@@ -91,9 +98,15 @@ public class EasonSimpleAuton extends OpMode {
     private void buildPath(){
         driveToTarget=follower.pathBuilder()
                 //Straight Line
-                .addPath(new BezierLine(DRIVE_START_POSE,TARGET_POSE))
+                .addPath(new BezierLine(DRIVE_START_POSE_1,TARGET_POSE_1))
                 //Maintain Heading
                 .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+        driveToTarget_2=follower.pathBuilder()
+                //Straight Line
+                .addPath(new BezierLine(DRIVE_START_POSE_2,TARGET_POSE_2))
+                //Maintain Heading
+                .setConstantHeadingInterpolation(Math.toRadians(-150))
                 .build();
     }
     //Update Machine States
@@ -115,10 +128,38 @@ public class EasonSimpleAuton extends OpMode {
             case START_DRIVE_TO_TARGET_1:
                 //Driving To Target
                 follower.followPath(driveToTarget,true);
-                autoState = AutoState.WAIT_FOR_DRIVE_TO_TARGET;
+                autoState = AutoState.WAIT_FOR_DRIVE_TO_TARGET_1;
                 break;
 
-            case WAIT_FOR_DRIVE_TO_TARGET:
+            case WAIT_FOR_DRIVE_TO_TARGET_1:
+                //Intake Start
+                intakeMotor.setVelocity(-INTAKE_VELOCITY);
+                // Wait Until Done With Driving
+                if (!follower.isBusy()){
+                    autoState = AutoState.START_TURN_2;
+                }
+                break;
+
+            case START_TURN_2:
+                //Turn to 180 degrees in PedroPathing
+                follower.turnTo( Math.toRadians(-150));
+                autoState = AutoState.WAIT_FOR_TURN_2;
+                break;
+
+            case WAIT_FOR_TURN_2:
+                //Wait Until Robot is Facing 180 Degrees
+                if (!follower.isBusy()){
+                    autoState = AutoState.START_DRIVE_TO_TARGET_2;
+                }
+                break;
+
+            case START_DRIVE_TO_TARGET_2:
+                //Driving To Target
+                follower.followPath(driveToTarget_2,true);
+                autoState = AutoState.WAIT_FOR_DRIVE_TO_TARGET_2;
+                break;
+
+            case WAIT_FOR_DRIVE_TO_TARGET_2:
                 //Intake Start
                 intakeMotor.setVelocity(-INTAKE_VELOCITY);
                 // Wait Until Done With Driving
