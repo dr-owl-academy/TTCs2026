772c024725315b69859d949564d96e4d397d6e97
danielthai <daniel.thai2011@gmail.com>
2026-07-29 12:05:31 -0700

    Working on new constant #5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
@@ -14,8 +15,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(7.05);
-
+    public static FollowerConstants followerConstants = new FollowerConstants().mass(7.05)
+            //.forwardZeroPowerAcceleration()
+            //.lateralZeroPowerAcceleration()
+            .headingPIDFCoefficients(new PIDFCoefficients(2,0,0.1,0.01))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.15,0,0.01,0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.6,0.000001,0.0001))
+            .centripetalScaling(0.0011);
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("frontRight")
@@ -26,7 +32,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(37.49650465597318);
+            .xVelocity(64.75662896263943)
+            .yVelocity(56.794285411284036);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(1.5)
@@ -41,8 +48,8 @@ public class Constants {
     public static PathConstraints pathConstraints = new PathConstraints(
             0.99,
             100,
-            0.27,
-            0.85);
+            0.9,
+            0.4);
 
 
     public static Follower createFollower(HardwareMap hardwareMap) {
