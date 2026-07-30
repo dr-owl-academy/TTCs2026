2bdb6bb0ee522b2de6686cc0d0286c42ff6a30d5
danielthai <daniel.thai2011@gmail.com>
2026-07-30 13:50:47 -0700

    Working on touch sensor #3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
@@ -18,15 +18,23 @@ public class TouchSensorPractice extends OpMode {
         Motor = hardwareMap.get(DcMotor.class, "motor");
         Motor.setPower(0);
         Motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
     public void loop() {
-        if (!bench.getTouchSensorState());
-        Motor.setPower(0.5);
+        if (!bench.getTouchSensorState()){
+            Motor.setPower(0.5);
+        } else{
+            Motor.setPower(0);
+        }
+        int currentPosition = Motor.getCurrentPosition();
+
+
 
         telemetry.addData("Touch Sensor State", bench.getTouchSensorState());
         telemetry.addData("Motor Power: ", Motor.getPower());
+        telemetry.addData("Times clicked: ", Motor.getCurrentPosition());
         telemetry.update();
     }
 }
