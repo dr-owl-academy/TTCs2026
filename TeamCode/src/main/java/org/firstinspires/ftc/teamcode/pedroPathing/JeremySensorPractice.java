67be5896a66dd4fecccae1727f1076842b1eb614
jeremy <jeremyyshen@gmail.com>
2026-07-30 13:16:42 -0700

    made motor flip directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/JeremySensorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/JeremySensorPractice.java
@@ -11,6 +11,12 @@ public class JeremySensorPractice extends OpMode {
     SimpleMbappeMechanism bench = new SimpleMbappeMechanism(); // make a new test bench instance
     private DcMotor motor = null; // define a motor
 
+    // define class variables
+    double motorVelocity = 0.3; // speed of motor
+    int sensorHoldTime = 0; // if held for enough time will turn off motor (3 second = 150 revolutions)
+    boolean isMotorSpinning = false;
+    int sensorClicks = 0; // how many times the sensor was clicked
+
     @Override
     public void init(){
         bench.init(hardwareMap);
@@ -22,12 +28,27 @@ public class JeremySensorPractice extends OpMode {
 
     @Override
     public void loop(){
+
+
         if(!bench.touchSensorState()){
-            motor.setPower(0.5);
+            isMotorSpinning = true; // make the motor spin
+            motorVelocity = -motorVelocity; // flip the direction
+            sensorHoldTime++; // add one to the time
+            sensorClicks++;
+
+            if(sensorHoldTime >= 150){
+                isMotorSpinning = false;
+            }
+        } else{
+            sensorHoldTime = 0; // reset time if released
+            if(isMotorSpinning){ // spin the motor only when told to
+                motor.setPower(motorVelocity);
+            }
         }
 
         telemetry.addData("Touch Sensor State: ", bench.touchSensorState());
         telemetry.addData("Motor Power: ", motor.getPower());
+        telemetry.addData("Sensor Clicks: ", sensorClicks);
         telemetry.update();
     }
 }
