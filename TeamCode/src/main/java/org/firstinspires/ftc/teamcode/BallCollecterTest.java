1ddb866e459254f5d8b5ad61a9dfbf169ab1d077
yajatpramod <yajatpramod1211@gmail.com>
2026-07-31 09:36:32 -0700

    almost done, ball collector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallCollecterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallCollecterTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -25,7 +26,12 @@ public class BallCollecterTest extends OpMode {
     SECOND BALL - THIRD BALL
     THIRD BALL - DEPOSIT
       */
-    STARTPOS_1MID
+        STARTPOS_1MID,
+        MID1_BALL1,
+        BALL1_MID2,
+        MID2_BALL2,
+        BALL2_BALL3,
+        BALL3_DEPOSIT
 
     }
 
@@ -36,8 +42,14 @@ public class BallCollecterTest extends OpMode {
     private final Pose ball1Pose = new Pose(77.4977220676374,71.41611221732335,Math.toRadians(180));
     private final Pose mid2Pose = new Pose(79.84632554962174,24.328636451650116,Math.toRadians(180));
     private final Pose ball2Pose = new Pose(27.4282145960422,21.987112844302306,Math.toRadians(180));
-
+    private final Pose ball3Pose = new Pose(27.4282145960422,21.987112844302306,Math.toRadians(0));
+    private final Pose deposit = new Pose(132.04214983276103,23.63395836848312,Math.toRadians(270));
     private PathChain startPoseMid1Pose;
+    private PathChain Mid1toBall1;
+    private PathChain Ball1toMid2;
+    private PathChain Mid2toBall2;
+    private PathChain Ball2toBall3;
+    private PathChain Ball3toDeposit;
 
     public void buildPaths() {
 
@@ -45,16 +57,74 @@ public class BallCollecterTest extends OpMode {
                 .addPath(new BezierCurve(startingPose,mid1Pose))
                 .setLinearHeadingInterpolation(startingPose.getHeading(),mid1Pose.getHeading())
                 .build();
+
+        Mid1toBall1 = follower.pathBuilder()
+                .addPath(new BezierCurve(mid1Pose,ball1Pose))
+                .setLinearHeadingInterpolation(mid1Pose.getHeading(),ball1Pose.getHeading())
+                .build();
+
+        Ball1toMid2 = follower.pathBuilder()
+                .addPath(new BezierCurve(ball1Pose,mid2Pose))
+                .setLinearHeadingInterpolation(ball1Pose.getHeading(),mid2Pose.getHeading())
+                .build();
+
+        Mid2toBall2 = follower.pathBuilder()
+                .addPath(new BezierCurve(mid2Pose,ball2Pose))
+                .setLinearHeadingInterpolation(mid2Pose.getHeading(),ball2Pose.getHeading())
+                .build();
+
+        Ball2toBall3 = follower.pathBuilder()
+                .addPath(new BezierCurve(ball2Pose,ball3Pose))
+                .setLinearHeadingInterpolation(ball2Pose.getHeading(),ball3Pose.getHeading())
+                .build();
+
+        Ball3toDeposit = follower.pathBuilder()
+                .addPath(new BezierCurve(ball3Pose,deposit))
+                .setLinearHeadingInterpolation(ball3Pose.getHeading(),deposit.getHeading())
+                .build();
+
     }
 
     public void statePathUpdate() {
         switch (pathState) {
             case STARTPOS_1MID:
-                follower.followPath(startPoseMid1Pose,true);
+                follower.followPath(startPoseMid1Pose, true);
+                pathState = PathState.STARTPOS_1MID;
+                break;
+
+            case MID1_BALL1:
+                if (!follower.isBusy())
+                    follower.followPath(Mid1toBall1,true);
+                    pathState = PathState.MID1_BALL1;
+                    break;
+
+            case BALL1_MID2:
+                if (!follower.isBusy())
+                    follower.followPath(Ball1toMid2);
+                    pathState = PathState.BALL1_MID2;
+                    break;
+
+            case MID2_BALL2:
+                if(!follower.isBusy())
+                    follower.followPath(Mid2toBall2);
+                    pathState = PathState.MID2_BALL2;
+                    break;
+
+            case BALL2_BALL3:
+                if(!follower.isBusy())
+                    follower.followPath(Ball2toBall3);
+                    pathState = PathState.BALL2_BALL3;
+                    break;
+
+            case BALL3_DEPOSIT:
+                if(!follower.isBusy())
+                    follower.followPath(Ball3toDeposit);
+                    pathState = PathState.BALL3_DEPOSIT;
+                    break;
+
+
+
         }
-
-
-
     }
 
 
@@ -66,6 +136,11 @@ public class BallCollecterTest extends OpMode {
         follower= Constants.createFollower(hardwareMap);
 
         follower.setStartingPose(startingPose);
+
+    }
+
+    public void start() {
+
 
     }
 
