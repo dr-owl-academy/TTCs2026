16d579be7c7ee6bf78a0f3b1dbcd1e6f53a31ece
danielthai <daniel.thai2011@gmail.com>
2026-07-31 10:04:21 -0700

    Working on touch sensor #5

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Sensorpracticestuff.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+
+@Autonomous(name = "AutoSensor")
+public class Sensorpracticestuff extends OpMode {
+    private Follower follower;
+    private PathChain driveToTarget;
+    private PathChain drivetoball1;
+    private PathChain drivetoball2;
+    private PathChain drivetoball3;
+    private PathChain Dropoffballscomplete;
+    private PathState pathState = PathState.Drive_to_Ball1;
+
+
+    private final Pose START_POSE = new Pose(37, 134, Math.toRadians(90));
+    private final Pose BALL1POSE = new Pose(24, 24, Math.toRadians(264));
+    private final Pose BALL2POSE = new Pose(128, 48, Math.toRadians(16));
+    private final Pose BALL3POSE = new Pose(79, 69, Math.toRadians(158));
+    private final Pose TARGET_POSE = new Pose(125, 30, Math.toRadians(315));
+
+    public enum PathState { //manage pathstates
+        Drive_to_Ball1,
+        Drive_to_Ball2,
+        Drive_to_Ball3,
+        Complete
+    }
+
+
+    @Override
+    public void init() {
+follower = Constants.createFollower(hardwareMap); //initiate follower object
+        follower.setStartingPose(START_POSE); //set initial position
+        follower.setMaxPower(1); //setting max speed
+        buildPath(); //build the bezier line path down there
+
+    }
+
+    @Override
+    public void loop() {
+
+        //Updates the PedroPathing
+        follower.update();
+
+        //Updates the autonomous for FSM
+        autonomousPathUpdate();
+
+        //Gets the pose for follower (know what coordinate it is)
+        Pose currentPose = follower.getPose();
+
+        //telemetry gives me the x and y positions on screen and other info
+        telemetry.addData("X", currentPose.getX());
+
+        telemetry.addData("Y", currentPose.getY());
+        telemetry.update();
+
+
+    }
+
+    private void buildPath() {
+
+
+        driveToTarget = follower.pathBuilder()
+                .addPath(new BezierLine(START_POSE, BALL1POSE))
+                        .setLinearHeadingInterpolation(START_POSE.getHeading(), BALL1POSE.getHeading())
+                                .build();
+        drivetoball1 = follower.pathBuilder()
+                .addPath(new BezierLine(BALL1POSE, BALL2POSE))
+                .setLinearHeadingInterpolation(BALL1POSE.getHeading(), BALL2POSE.getHeading())
+                .build();
+        drivetoball2 = follower.pathBuilder()
+                .addPath(new BezierLine(BALL2POSE, BALL3POSE))
+                .setLinearHeadingInterpolation(BALL2POSE.getHeading(), BALL3POSE.getHeading())
+                .build();
+
+    }
+
+    private void autonomousPathUpdate() {
+        switch (pathState) {
+
+            case Drive_to_Ball1:
+                if (!follower.isBusy()) {
+                    pathState = PathState.Drive_to_Ball2;
+                }
+                break;
+            case Drive_to_Ball2:
+                if (!follower.isBusy()) {
+                    pathState = PathState.Drive_to_Ball3;
+                }
+                break;
+            case Drive_to_Ball3:
+                if (!follower.isBusy()) {
+                    pathState = PathState.Complete;
+                }
+                break;
+            case Complete:
+                break;
+        }
+    }
+}
