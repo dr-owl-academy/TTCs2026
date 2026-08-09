f8fc8d84c56da1b55847f2b1b109ea5820e40128
IMMORTL225 <lilsci2013@gmail.com>
2026-08-09 10:27:07 -0700

    Matthews Code commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -19,11 +19,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(5.15)
-          //  .forwardZeroPowerAcceleration(-34.34415518547419)
-          //  .lateralZeroPowerAcceleration(-54.76122077)
-          //  .translationalPIDFCoefficients(new PIDFCoefficients(0.15, 0, 0.015, 0.01))
-          //  .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0.01))
-          //  .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.000001,0.6, 0.0001));
+          //  .forwardZeroPowerAcceleration(-)
+          //  .lateralZeroPowerAcceleration(-54)76122077
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.06, 0, 0.0034, 0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.001, 0.025))
+          //x  .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.000001,0.6, 0.0001));
     ;
 
 
