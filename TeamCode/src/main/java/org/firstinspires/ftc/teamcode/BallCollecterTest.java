aa4404ee5c61cdb4ffa73c49bedcdf21bcce4602
yajatpramod <yajatpramod1211@gmail.com>
2026-08-16 11:02:09 -0700

    BallCollecterTest updated 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallCollecterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallCollecterTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
@@ -32,6 +33,7 @@ public class BallCollecterTest extends OpMode {
         MID2_BALL2,
         BALL2_BALL3,
         BALL3_DEPOSIT,
+        WAIT_FOR_DEPOSIT,
         COMPLETE
 
     }
@@ -43,8 +45,9 @@ public class BallCollecterTest extends OpMode {
     private final Pose ball1Pose = new Pose(77.4977220676374,71.41611221732335,Math.toRadians(180));
     private final Pose mid2Pose = new Pose(79.84632554962174,24.328636451650116,Math.toRadians(180));
     private final Pose ball2Pose = new Pose(27.4282145960422,21.987112844302306,Math.toRadians(180));
-    private final Pose ball3Pose = new Pose(27.4282145960422,21.987112844302306,Math.toRadians(0));
+    private final Pose ball3Pose = new Pose(134.43833943833945,35.81623931623931,Math.toRadians(0));
     private final Pose deposit = new Pose(132.04214983276103,23.63395836848312,Math.toRadians(270));
+
     private PathChain startPoseMid1Pose;
     private PathChain Mid1toBall1;
     private PathChain Ball1toMid2;
@@ -55,32 +58,32 @@ public class BallCollecterTest extends OpMode {
     public void buildPaths() {
 
         startPoseMid1Pose = follower.pathBuilder()
-                .addPath(new BezierCurve(startingPose,mid1Pose))
+                .addPath(new BezierLine(startingPose,mid1Pose))
                 .setLinearHeadingInterpolation(startingPose.getHeading(),mid1Pose.getHeading())
                 .build();
 
         Mid1toBall1 = follower.pathBuilder()
-                .addPath(new BezierCurve(mid1Pose,ball1Pose))
+                .addPath(new BezierLine(mid1Pose,ball1Pose))
                 .setLinearHeadingInterpolation(mid1Pose.getHeading(),ball1Pose.getHeading())
                 .build();
 
         Ball1toMid2 = follower.pathBuilder()
-                .addPath(new BezierCurve(ball1Pose,mid2Pose))
+                .addPath(new BezierLine(ball1Pose,mid2Pose))
                 .setLinearHeadingInterpolation(ball1Pose.getHeading(),mid2Pose.getHeading())
                 .build();
 
         Mid2toBall2 = follower.pathBuilder()
-                .addPath(new BezierCurve(mid2Pose,ball2Pose))
+                .addPath(new BezierLine(mid2Pose,ball2Pose))
                 .setLinearHeadingInterpolation(mid2Pose.getHeading(),ball2Pose.getHeading())
                 .build();
 
         Ball2toBall3 = follower.pathBuilder()
-                .addPath(new BezierCurve(ball2Pose,ball3Pose))
+                .addPath(new BezierLine(ball2Pose,ball3Pose))
                 .setLinearHeadingInterpolation(ball2Pose.getHeading(),ball3Pose.getHeading())
                 .build();
 
         Ball3toDeposit = follower.pathBuilder()
-                .addPath(new BezierCurve(ball3Pose,deposit))
+                .addPath(new BezierLine(ball3Pose,deposit))
                 .setLinearHeadingInterpolation(ball3Pose.getHeading(),deposit.getHeading())
                 .build();
 
@@ -124,9 +127,14 @@ public class BallCollecterTest extends OpMode {
             case BALL3_DEPOSIT:
                 if (!follower.isBusy()){
                     follower.followPath(Ball3toDeposit);
-                pathState = PathState.COMPLETE;
+                pathState = PathState.WAIT_FOR_DEPOSIT;
                 }
                 break;
+
+            case WAIT_FOR_DEPOSIT:
+                if(!follower.isBusy()){
+                    pathState = PathState.COMPLETE;
+                }
 
             case COMPLETE:
                 break;
