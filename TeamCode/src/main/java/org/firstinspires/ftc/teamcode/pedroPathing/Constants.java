4455ae9635cbf73b2c9865100c6b89d29fc0b7f8
danielthai <daniel.thai2011@gmail.com>
2026-08-16 10:35:59 -0700

    Fixing auton to make it run properly (Changing constants and names, fixing tuning)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -15,18 +15,18 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {public static FollowerConstants followerConstants = new FollowerConstants().mass(5.15)
-            .forwardZeroPowerAcceleration(-28.271419412264098)
-            .lateralZeroPowerAcceleration(-45.13263414936911)
-            .headingPIDFCoefficients(new PIDFCoefficients(2,0,0.1,0.01))
+            .forwardZeroPowerAcceleration(-38.7888036454501107)
+            .lateralZeroPowerAcceleration(-54.1272983576398125)
+            .headingPIDFCoefficients(new PIDFCoefficients(1,0,0.61,0.02))
             .translationalPIDFCoefficients(new PIDFCoefficients(0.06,0,0.0034,0.02))
             .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.000001,0.6,0.0001))
-            .centripetalScaling(0.0011);
+            .centripetalScaling(0.00059);
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
-            .rightFrontMotorName("frontRight")
-            .rightRearMotorName("backRight")
-            .leftRearMotorName("backLeft")
-            .leftFrontMotorName("frontLeft")
+            .rightFrontMotorName("motorRF")
+            .rightRearMotorName("motorRB")
+            .leftRearMotorName("motorLB")
+            .leftFrontMotorName("motorLF")
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
@@ -47,8 +47,8 @@ public class Constants {public static FollowerConstants followerConstants = new 
     public static PathConstraints pathConstraints = new PathConstraints(
             0.99,
             100,
-            0.9,
-            0.4);
+            1.5,
+            0.5);
 
 
     public static Follower createFollower(HardwareMap hardwareMap) {
