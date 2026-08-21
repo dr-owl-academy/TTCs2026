67fd999b79c22718cd92e85d94fcba2f0e211898
danielthai <daniel.thai2011@gmail.com>
2026-08-20 17:53:36 -0700

    Sensor practice auton edit #3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Sensorpracticestuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Sensorpracticestuff.java
@@ -22,7 +22,7 @@ public class Sensorpracticestuff extends OpMode {
 
     private final Pose START_POSE = new Pose(37, 134, Math.toRadians(-90));
     private final Pose BALL1POSE = new Pose(24, 24, Math.toRadians(264));
-    private final Pose BALL2POSE = new Pose(128, 48, Math.toRadians(16));
+    private final Pose BALL2POSE = new Pose(121, 48, Math.toRadians(16));
     private final Pose BALL3POSE = new Pose(79, 69, Math.toRadians(158));
     private final Pose TARGET_POSE = new Pose(125, 30, Math.toRadians(315));
 
