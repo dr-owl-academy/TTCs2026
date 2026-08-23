5c4323c2fb8aaf71b21e0b493feb9abb32086759
SomeGuyOnline66 <duyifei5846@students.esuhsd.org>
2026-08-23 10:31:21 -0700

    Written code in AimeeLimeLight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimeeLimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimeeLimeLight.java
@@ -1,4 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AimeeLimeLight {
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Green Ball Motor Test")
+public class AimeeLimeLight extends OpMode {
+
+    private DcMotor testMotor;
+
+    private Limelight3A limelight;
+
+    //
+    private static final int GREEN_BALL_PIPELINE = 9;
+
+    private static final double MOTOR_POWER = 0.2;
+
+
+    @Override
+    public void init() {
+
+        //Motor
+        testMotor = hardwareMap.get(
+                DcMotor.class,
+                "testMotor"
+        );
+
+        testMotor.setDirection(
+                DcMotor.Direction.FORWARD
+        );
+
+        testMotor.setPower(0);
+    }
 }
